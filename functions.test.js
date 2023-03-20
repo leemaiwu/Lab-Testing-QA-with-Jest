8d7eb93b0d6dcf254c6a93025114627937e05f8c
Laura Wu <leemaiwu@gmail.com>
2023-03-20 16:43:30 -0600

    Completed extra challenge section

--- functions.test.js
+++ functions.test.js
@@ -1,6 +1,9 @@
 const { returnTwo,
         greeting,
         add,
+        multiply,
+        divide,
+        subtract
     } = require('./functions.js')
 
 test('returnTwo should return the number 2', () => {
@@ -13,4 +16,16 @@ test('greeting should return a string greeting the inputed name', () => {
 
 test('add should return the sum of two inputed numbers', () => {
     expect(add(1, 2)).toEqual(3)
+})
+
+test('multiply should give the product of two inputed numbers', () => {
+    expect(multiply(2, 2)).toEqual(4)
+})
+
+test('divide should return num1 divided by num2', () => {
+    expect(divide(4, 4)).toEqual(1)
+})
+
+test('subtract should give the difference', () => {
+    expect(subtract(2, 2)).toEqual(0)
 })
